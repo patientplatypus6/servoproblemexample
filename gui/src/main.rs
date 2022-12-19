01c70e3b6b24195b921089d4e772b277b7938a71
System Administrator <root@Peters-MacBook-Pro.local>
2022-12-19 13:49:47 -0800

    bidirectional communication now works

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -6,16 +6,22 @@ use std::io;
 use std::{thread, time};
 
 type Data = Vec<(String, String)>;
+type Bootstrap = (IpcSender<Data>, IpcReceiver<Data>);
 
 fn main() {
     println!("Inside GUI process");
     let args: Vec<String> = env::args().collect();
-    let (s1, r1): (IpcSender<Data>, IpcReceiver<Data>) = ipc::channel().unwrap();
-    let s0 = IpcSender::connect(args[1].clone()).unwrap();
-    s0.send(s1).unwrap();
+    println!("value of args {:?}", args);
+    let (to_child, from_parent): (IpcSender<Data>, IpcReceiver<Data>) = ipc::channel().unwrap();
+    let (to_parent, from_child): (IpcSender<Data>, IpcReceiver<Data>) = ipc::channel().unwrap();
+    let bootstrap = IpcSender::connect(args[1].clone()).unwrap();
+    bootstrap.send((to_child, from_child)).unwrap();
+    let data = from_parent.recv().unwrap();
+    println!("child received {:?}", data);
+    to_parent.send(vec![("Dagne".to_string(), "8".to_string())]);
 
     loop {
-        match r1.try_recv() {
+        match from_parent.try_recv() {
             Ok(res) => {
                 println!("Received data in gui...{:?}", res);
                 break;
--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,22 @@ use std::{thread, time};
 
 // type Data = (String, String);
 type Data = Vec<(String, String)>;
+type Bootstrap = (IpcSender<Data>, IpcReceiver<Data>);
 
 fn main() {
     println!("Inside process_handler");
-    let (server0, server_name0) = IpcOneShotServer::<IpcSender<Data>>::new().unwrap();
+    let (server0, server_name0) = IpcOneShotServer::<Bootstrap>::new().unwrap();
     println!("server_name0 in process_handler: {:?}", server_name0);
     let guiserver = spawn_server(
         "/Users/peterweyand/Code/rustprojects/project1_2/src/rungui.sh".to_string(),
         &server_name0
     );
-    let (_receiver, sender): (IpcReceiver<IpcSender<Data>>, IpcSender<Data>) = server0.accept().unwrap();
-
+    let (_receiver, (sender, receiver)): (IpcReceiver<Bootstrap>, Bootstrap) = server0.accept().unwrap();
     let data = vec![("Peter".to_string(), "36".to_string())];
-    println!("value of _receiver {:?}", _receiver);
     sender.send(data);
 
     loop {
-        match _receiver.try_recv() {
+        match receiver.try_recv() {
             Ok(res) => {
                 println!("Received data in main...{:?}", res);
                 break;
@@ -35,9 +34,9 @@ fn main() {
     }
 }
 
-pub fn spawn_server(address:String, test_name: &str) -> std::process::Child {
+pub fn spawn_server(address:String, server_name0: &str) -> std::process::Child {
     Command::new(address) 
-        .arg(test_name)
+        .arg(server_name0)
         .spawn()
         .expect("failed to execute server process")
 }
